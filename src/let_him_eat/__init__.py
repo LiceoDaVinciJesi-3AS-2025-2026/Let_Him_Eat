33573aa8eaa5f0f0e160400b8237170838bc5d93
Andrea Cittadini <andreacittadini7@gmail.com>
2026-02-18 18:43:40 +0100

    pergamena
    ho aggiunto la pergamena con le regole e ho sistemato le dimensioni di gartfield

--- src/let_him_eat/__init__.py
+++ src/let_him_eat/__init__.py
@@ -18,14 +18,20 @@ def main() -> None:
     x = SCREEN_WIDTH // 2
     y = SCREEN_HEIGHT // 2
     
+    imgRegole = pygame.image.load("pergamenaConRegole.png") 
+    imgRegole = pygame.transform.scale(imgRegole,(SCREEN_WIDTH // 2.5, SCREEN_HEIGHT // 1.40))
+    
+    x = SCREEN_WIDTH 
+    y = SCREEN_HEIGHT 
+    
    #scritte
     Titlefont = pygame.font.SysFont('Impact', 70)
     Subtitlefont = pygame.font.SysFont('Impact', 30)
     Normalfont = pygame.font.SysFont('Serif', 30)
     game_start = Titlefont.render("Benvenuto in Let Him Eat!", True, "dark red")
     subtitle = Subtitlefont.render("Aiuta garfield a mangiare le sue amate lasagne", True, "dark red")
-    commands = Normalfont.render("Ricordarsi di aggiungere i comandi:)", True, "blue" )
-    start_text = Normalfont.render("Benvenuto nel gioco! Prima di premere Invio e iniziare l’avventura lascia che ti sveli le regole fondamentali per affrontare la sfida al meglio!", True, "dark gray")
+#     commands = Normalfont.render("Ricordarsi di aggiungere i comandi:)", True, "blue" )
+#     start_text = Normalfont.render("Benvenuto nel gioco! Prima di premere Invio e iniziare l’avventura lascia che ti sveli le regole fondamentali per affrontare la sfida al meglio!", True, "dark gray")
 
    
    #pulsante
@@ -37,7 +43,7 @@ def main() -> None:
     playerX = 400
     playerY = 300
     player_speed = 5
-    player_size = 80
+    player_size = 50
     
     imgGarfield = pygame.image.load("garfield_senza_sfondo.png")
     imgGarfield = pygame.transform.scale(imgGarfield,(player_size, player_size))
@@ -70,10 +76,11 @@ def main() -> None:
         # se ci troviamo nella schermata iniziale
         if home:
             screen.blit(imgSfondo,(0,0))
+            screen.blit(imgRegole,(90,170))
             screen.blit(game_start, (80,50))
             screen.blit(subtitle, (80,125))
-            screen.blit(commands, (80,170))
-            screen.blit(start_text, (80, 200))
+#             screen.blit(commands, (80,170))
+#             screen.blit(start_text, (80, 200))
         
             mPos = pygame.mouse.get_pos()
         
